2c331364e1c5c839cc78d57ac7717b7facd291b4
ValDeCans <valdecans.dev@gmail.com>
2023-10-27 09:48:03 -0300

    Multiple Components Created a reusable component that accepts inputs. Association with child component.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,12 +2,13 @@ import 'package:ngdart/angular.dart';
 import 'package:ngforms/ngforms.dart';
 import 'src/hero.dart';
 import 'src/mock_heroes.dart';
+import 'src/hero_component.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: 'app_component.html',
   styleUrls: ['app_component.css'],
-  directives: [coreDirectives, formDirectives],
+  directives: [coreDirectives, HeroComponent],
 )
 class AppComponent {
   final title = 'Viagem dos Heróis';
---
+++ lib/src/hero_component.dart
@@ -0,0 +1,23 @@
+import 'package:ngdart/angular.dart';
+import 'package:ngforms/ngforms.dart';
+import 'hero.dart';
+
+@Component(
+  selector: 'my-hero',
+  template: '''
+    <div *ngIf="hero != null">
+      <h2>{{hero!.name}}</h2>
+      <div><label>id: </label>{{hero!.id}}</div>
+      <div>
+          <label>name:
+              <input [(ngModel)]="hero!.name" placeholder="name">
+          </label>
+      </div>
+    </div>
+  ''',
+  directives: [coreDirectives, formDirectives],
+)
+class HeroComponent {
+  @Input()
+  Hero? hero;
+}
