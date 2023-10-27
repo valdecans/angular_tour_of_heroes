f7b5ca8bb6771d9e14d9d24eee3c68e09c3eeb55
ValDeCans <valdecans.dev@gmail.com>
2023-10-27 06:20:33 -0300

    The Hero Editor Modified the home application to display more interesting data and allow the user to edit this data.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -1,9 +1,21 @@
 import 'package:ngdart/angular.dart';
+import 'package:ngforms/ngforms.dart';
+import 'src/hero.dart';
 
 @Component(
   selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
+  template: '''
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}}</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+  ''',
+  directives: [formDirectives],
 )
 class AppComponent {
-  var name = 'Angular';
+  final title = 'Viagem dos Heróis';
+  Hero hero = Hero(1, 'Vendaval');
 }
