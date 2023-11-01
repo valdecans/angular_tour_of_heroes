bfbd3bdba562aad5f7e5106d3e50a19fa3ed7777
ValDeCans <valdecans.dev@gmail.com>
2023-10-31 22:20:12 -0300

    Services II Created a service class. Used lifecycle hook to get data. (Correction code error).

---
+++ lib/src/hero_list_component.dart
@@ -0,0 +1,27 @@
+import 'package:ngdart/angular.dart';
+import 'package:ngforms/ngforms.dart';
+import 'hero.dart';
+import 'mock_heroes.dart';
+import 'hero_component.dart';
+import 'hero_service.dart';
+
+@Component(
+  selector: 'my-heroes',
+  templateUrl: 'hero_list_component.html',
+  styleUrls: ['hero_list_component.css'],
+  directives: [coreDirectives, HeroComponent],
+)
+class HeroListComponent implements OnInit {
+  Hero? selected;
+
+  List<Hero> heroes = <Hero>[];
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAllSlowly();
+  }
+
+  final HeroService _heroService;
+  HeroListComponent(this._heroService);
+
+  void onSelect(Hero hero) => selected = hero;
+  void ngOnInit() => _getHeroes();
+}
